8618a7db24c9290e7c52e7ceaf258b84f62a2ca1
AnJicheol <kany0511@gmail.com>
2025-12-31 10:49:29 +0900

    init

---
+++ apps/announcements/src/test/java/org/example/announcements/AnnouncementsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.announcements;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnnouncementsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ apps/policies/src/test/java/org/example/policies/PoliciesApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.policies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PoliciesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
