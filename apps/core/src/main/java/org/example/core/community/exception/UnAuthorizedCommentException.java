e20495f2a3dc05112af11bb44bee41596a5520f8
nornsong <ahg077126@gmail.com>
2026-01-12 10:05:11 +0900

    feat: 커뮤니티 예외 추가

---
+++ apps/core/src/main/java/org/example/core/community/exception/UnAuthorizedCommentException.java
@@ -0,0 +1,7 @@
+package org.example.core.community.exception;
+
+public class UnAuthorizedCommentException extends RuntimeException {
+    public UnAuthorizedCommentException(String message) {
+        super(message);
+    }
+}
