1f57b94d9cb249b258eb00fdb882e5a847ff8560
1ShinSuHo1 <sshsh1004@naver.com>
2026-01-14 01:47:04 +0900

    fix: keyset 기반 스크롤 쿼리 오류 수정

--- apps/announcements/src/main/java/org/example/announcements/repository/AnnouncementRepository.java
+++ apps/announcements/src/main/java/org/example/announcements/repository/AnnouncementRepository.java
@@ -15,30 +15,18 @@ public interface AnnouncementRepository extends JpaRepository<Announcement, Long
     //파싱서버에서 넘어온 공고를 db에 저장하기 위해 사용
     Optional<Announcement> findBySourceAndExternalKey(AnnouncementSource source, String externalKey);
 
-    // 접수중(open) 공고 조회 + 최신순으로 조회하기
-    @Query("""
-        select a
-        from Announcement a
-        where a.startDate <= :today
-          and a.endDate >= :today
-        order by a.createdAt desc, a.id desc
-    """)
-    Window<Announcement> scrollOpenLatest(
-            @Param("today") LocalDate today,
+    // 접수중(open) + 최신순(createdAt desc, id desc)
+    Window<Announcement> findByStartDateLessThanEqualAndEndDateGreaterThanEqualOrderByCreatedAtDescIdDesc(
+            LocalDate today1,
+            LocalDate today2,
             KeysetScrollPosition position,
             Limit limit
     );
 
-    // 접수중(open) 공고 조회 + 마감임박순으로 조회하기
-    @Query("""
-        select a
-        from Announcement a
-        where a.startDate <= :today
-          and a.endDate >= :today
-        order by a.endDate asc, a.id desc
-    """)
-    Window<Announcement> scrollOpenDeadline(
-            @Param("today") LocalDate today,
+    // 접수중(open) + 마감임박순(endDate asc, id desc)
+    Window<Announcement> findByStartDateLessThanEqualAndEndDateGreaterThanEqualOrderByEndDateAscIdDesc(
+            LocalDate today1,
+            LocalDate today2,
             KeysetScrollPosition position,
             Limit limit
     );
