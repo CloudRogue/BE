b0b175e505304f23c1c2bc28d559f18b3596d982
1ShinSuHo1 <sshsh1004@naver.com>
2026-01-20 17:18:52 +0900

    feat: 신청관리(APPLYING) 조회용 도메인 및 Querydsl 조회 구현

--- apps/announcements/src/main/java/org/example/announcements/repository/AnnouncementApplicationRepository.java
+++ apps/announcements/src/main/java/org/example/announcements/repository/AnnouncementApplicationRepository.java
@@ -2,7 +2,9 @@ package org.example.announcements.repository;
 
 import org.example.announcements.domain.AnnouncementApplication;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface AnnouncementApplicationRepository extends JpaRepository<AnnouncementApplication, Long> ,AnnouncementApplicationManageQueryRepository{
 
     //중복 지원 방어 체크용
