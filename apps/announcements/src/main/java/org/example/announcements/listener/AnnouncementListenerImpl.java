5036e2d953b60b2aa2911de702e390c5d141a8c2
1ShinSuHo1 <sshsh1004@naver.com>
2026-01-22 02:30:54 +0900

    feat: 공고 조회 리스너 구현

--- apps/announcements/src/main/java/org/example/announcements/listener/AnnouncementListenerImpl.java
+++ apps/announcements/src/main/java/org/example/announcements/listener/AnnouncementListenerImpl.java
@@ -1,18 +1,33 @@
 package org.example.announcements.listener;
 
+import lombok.RequiredArgsConstructor;
 import org.example.announcements.domain.Announcement;
+import org.example.announcements.exception.BusinessException;
+import org.example.announcements.exception.ErrorCode;
+import org.example.announcements.repository.AnnouncementRepository;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
 
 @Component
+@RequiredArgsConstructor
 public class AnnouncementListenerImpl implements AnnouncementListener{
 
+    private final AnnouncementRepository announcementRepository;
 
     @Override
     public List<Announcement> getNewAnnouncement() {
-        return List.of();
+        //어드민 검수 안된 공고만 조회
+        return announcementRepository.findByAdminCheckedFalse();
     }
 
-    public Announcement getAnnouncement(Long announcementId){return null;}
+
+    // 아이디랑 검수트루인 공고만 단건조회
+    public Announcement getAnnouncement(Long announcementId){
+        return announcementRepository.findByIdAndAdminCheckedTrue(announcementId)
+                .orElseThrow(() -> new BusinessException(
+                        ErrorCode.ANNOUNCEMENT_NOT_FOUND,
+                        "검수된 공고를 찾을 수 없습니다. announcementId=" + announcementId
+                ));
+    }
 }
