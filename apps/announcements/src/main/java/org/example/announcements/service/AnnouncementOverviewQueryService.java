d68495ecb77ffd4e74acf834e6cf9718ba95bdf4
AnJicheol <kany0511@gmail.com>
2026-01-26 21:26:40 +0900

    .

--- apps/announcements/src/main/java/org/example/announcements/service/AnnouncementOverviewQueryService.java
+++ apps/announcements/src/main/java/org/example/announcements/service/AnnouncementOverviewQueryService.java
@@ -4,6 +4,7 @@ import org.example.announcements.dto.AnnouncementOverviewResponse;
 
 public interface AnnouncementOverviewQueryService {
 
+
     // 공고 개요 조회
     AnnouncementOverviewResponse getOverview(Long announcementId);
 }
